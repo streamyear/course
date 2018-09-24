d1fec5c75b6b4560a67cd7cfafd78136ad30756a
Beck <2500931932@qq.com>
2018-09-24 14:15:44 +0800

    SpringBoot框架搭建系列(三)：解决日期转换问题和日期展示问题

--- src/main/java/com/streamyear/course/controller/IndexController.java
+++ src/main/java/com/streamyear/course/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.streamyear.course.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 /**
  * 根目录的Controller
  */
@@ -12,6 +14,16 @@ public class IndexController {
     @RequestMapping("/")
     public String index(){
         return "欢迎您,访问SpringBoot项目!";
+    }
+
+    /**
+     * 测试日期
+     * @param joinDay
+     * @return
+     */
+    @RequestMapping("date")
+    public Date testDate(Date joinDay){
+        return joinDay;
     }
 
 }
